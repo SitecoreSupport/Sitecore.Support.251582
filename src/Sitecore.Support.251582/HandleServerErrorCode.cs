ff39be5209862951af943a4bd24aa9b4adbf74d6
DK\EVS <evs@sitecore.com>
2018-08-31 13:39:53 +0300

    patched the HandleServerErrorCode class to use reflection to get the field's value

---
+++ src/Sitecore.Support.251582/HandleServerErrorCode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Web;
+
+namespace Sitecore.Support.XA.Feature.ErrorHandling.Pipelines.HtpRequestProcessed
+{
+  public class HandleServerErrorCode: Sitecore.XA.Feature.ErrorHandling.Pipelines.HtpRequestProcessed.HandleServerErrorCode
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+      if (args.Context.Error != null && !Context.Site.Name.Equals("shell"))
+      {
+        FieldInfo field = typeof(Sitecore.Sites.SiteContext).GetField
+          ("info", BindingFlags.Instance | BindingFlags.NonPublic);
+        SiteInfo siteInfo = (field==null)?null:field.GetValue(Context.Site) as SiteInfo;
+        if (siteInfo != null)
+        {
+          string staticErrorPageUrl = GetStaticErrorPageUrl(siteInfo);
+          if (File.Exists(FileUtil.MapPath(staticErrorPageUrl)))
+          {
+            args.Context.Server.TransferRequest(staticErrorPageUrl);
+          }
+          else
+          {
+            Log.Warn($"Could not find proper static error page for site: {siteInfo.Name}. Please generate it.", this);
+          }
+        }
+      }
+    }
+  }
+}
