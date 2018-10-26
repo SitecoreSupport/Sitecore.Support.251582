1c2d4bfe43a9c9f22026d24351d9418d38d78e16
DK\IRO <iro@sitecore.net>
2018-10-26 13:15:47 +0300

    Initial

---
+++ src/Sitecore.Support.251582/SiteInfoResolver.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.XA.Foundation.Multisite
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Abstractions;
+  using Sitecore.DependencyInjection;
+  using Sitecore.Web;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class SiteInfoResolver : Sitecore.XA.Foundation.Multisite.SiteInfoResolver
+  {
+
+    private IEnumerable<SiteInfo> _sites;
+
+    public override IEnumerable<SiteInfo> Sites
+    {
+      get
+      {
+        BaseSiteContextFactory service = ServiceLocator.ServiceProvider.GetService<BaseSiteContextFactory>();
+        return _sites ?? (_sites = from s in service.GetSites()
+                                   where s.EnablePreview
+                                   orderby s.RootPath descending
+                                   select s);
+      }
+    }
+  }
+}
