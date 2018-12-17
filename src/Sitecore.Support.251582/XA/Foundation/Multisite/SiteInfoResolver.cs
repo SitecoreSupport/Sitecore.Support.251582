dd66073d32a808624a4221612f2da704a5df959c
NikolayPautov <npa@sitecore.net>
2018-12-17 15:15:04 +0200

    Merge both scenarios and make compatible with 1.7.0-9.01

---
+++ src/Sitecore.Support.251582/XA/Foundation/Multisite/SiteInfoResolver.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.DependencyInjection;
+using Sitecore.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Sitecore.Support.XA.Foundation.Multisite
+{
+ 
+  public class SiteInfoResolver : Sitecore.XA.Foundation.Multisite.SiteInfoResolver
+  {
+
+    private IEnumerable<SiteInfo> _sites;
+
+    public override IEnumerable<SiteInfo> Sites
+    {
+      get
+      {
+        BaseSiteContextFactory service = ServiceLocator.ServiceProvider.GetService<BaseSiteContextFactory>();
+        return _sites ?? (_sites = from s in service.GetSites()
+                                   where s.Properties.AllKeys.Contains("IsSxaSite")
+                                   orderby s.RootPath descending
+                                   select s);
+      }
+    }
+  }
+
+}
